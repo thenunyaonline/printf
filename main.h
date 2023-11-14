cacc8ec552b7b7ccd07ed739de7c9039825a3588
theGraye <niimartei24@gmail.com>
2023-11-14 14:46:29 -0800

    printf

--- main.h
+++ main.h
@@ -13,16 +13,11 @@ int printf_string(va_list val);
 int print_37(void);
 int _printf(const char *format, ...);
 int _strlen(char *str);
-int print_in(va_list arg);
-int print_de(va_list arg);
-<<<<<<< HEAD
 int print_int(va_list args);
 int print_dec(va_list args);
-=======
 int binary(int num);
 int octal(int num);
 int _MAX;
 int _MIN;
->>>>>>> f5e6ce0425becaa2b55c2e37e03e65bda9df5c97
 
 #endif
